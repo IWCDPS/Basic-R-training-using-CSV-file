e4e12b5d222358921fd5f00f2c13c3184bda3457
IWCDPS <146726937+IWCDPS@users.noreply.github.com>
2024-11-03 20:07:35 +0530

    Add files via upload

---
+++ Basic R trainig using CSV file.R
@@ -0,0 +1,44 @@
+set directory
+p<-read.csv("df.csv")
+p
+View(p)
+
+#SUMMARY
+summary(p)
+
+#STANDARD DEVIATION
+sd(p$tmax)
+sd(p$tmax)
+sd(p$tmin)
+
+#MEAN
+mean(p$tmax)
+mean(p$tmin
+     
+#PLOT
+plot(p)
+#HISTOGRAM
+hist(p$tmax)
+hist(p$time)
+hist(p$tmax, main="My Graph", xlab="The x-axis", ylab="The y axis")
+hist(p$tmin, main="My Graph", xlab="The x-axis", ylab="The y axis",col="green")
+
+#BOXPLOT
+boxplot(p$tmax)
+boxplot(p$tmin, main="My Graph", xlab="The x-axis", ylab="The y axis",col="green")
+
+#BARPLOT
+barplot(p$tmax)
+barplot(p$tmax, main="My Graph", xlab="The x-axis", ylab="The y axis",col="green")
+library(ggplot2)
+ggplot(p, aes(time, tmax)) +
+  geom_point() +
+  geom_line() +
+  labs(x = "time", y = "tmax", 
+  title = "tmax", color="red")
+
+
+
+
+
+
